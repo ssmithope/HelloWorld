8baf75000f010348b3c98cd7b59a50c8d8d5bfdf
ssmithope <ssmithope@gmail.com>
2026-05-07 23:00:19 -0500

    first commit

--- obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a386cd0811e33fe8adb517e4a451d9c3d65b7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf55a1de2b80dc04defaeb5ab6d71b5f7671e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
